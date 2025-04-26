05376ab49529333f38af7cd8fcca50294ada2a92
manua4 <manuthi.abey@gmail.com>
2025-04-26 16:58:34 +0530

    manu

---
+++ frontend/src/Components/tickets/ticketNav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ticket.css';
+
+function TicketNav() {
+  return (
+    <nav>
+      <h2>Ticket Raising Platform</h2>
+      <button>Create Ticket</button>
+    </nav>
+  );
+}
+
+export default TicketNav;
